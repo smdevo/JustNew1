dc1d1b0a3d6c36c2ad40cb2536cb3af120968314
Samandar <samandartoshpulatov0704@gmail.com>
2024-06-23 22:52:37 +0500

    Initial Commit

---
+++ JustNew1/JustNew1App.swift
@@ -0,0 +1,17 @@
+//
+//  JustNew1App.swift
+//  JustNew1
+//
+//  Created by Samandar on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JustNew1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
